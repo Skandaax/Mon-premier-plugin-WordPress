65ccfc9a47f01c67c1244e5b0b01647889ffea38
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-06-03 08:28:49 +0200

    Ajout d'un commentaire d'activation

---
+++ mon_premier_plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Mon premier plugin !
+Plugin URI: https:/www.inforaz.com/
+Description: Mon prmier plugin.
+Author: Couillin Yannick
+Version 1.0
+Author:URI: http:/www.inforaz.com/
+Licence: GPLv2
+*/
+
